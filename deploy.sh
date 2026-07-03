3e3aa006ec70bdfae6ed4d23157b6d3c19a30d1d
edualc <royalhell118@gmail.com>
2026-07-03 16:34:47 +0200

    build: clean deploy dir before packing

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ DEPLOY_DIR="$(dirname "$SCRIPT_DIR")/deploy"
 cd "$SCRIPT_DIR"
 
 mkdir -p "$DEPLOY_DIR"
+rm -f "$DEPLOY_DIR"/*.zip
 
 for dir in */; do
     dir="${dir%/}"
     [[ -f "$dir/info.json" ]] || continue
-    rm -f "$DEPLOY_DIR/${dir}.zip"
     python3 -c "import shutil; shutil.make_archive('$DEPLOY_DIR/${dir}', 'zip', '.', '${dir}')"
     echo "Packed $DEPLOY_DIR/${dir}.zip"
 done
