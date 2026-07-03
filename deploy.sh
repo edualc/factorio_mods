754a9be99d06a9dfa2f0fe41d99c8a1cb7eb1565
edualc <royalhell118@gmail.com>
2026-07-03 16:40:17 +0200

    build: use powershell Compress-Archive instead of zip/python

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ rm -f "$DEPLOY_DIR"/*.zip
 for dir in */; do
     dir="${dir%/}"
     [[ -f "$dir/info.json" ]] || continue
-    python3 -c "import shutil; shutil.make_archive('$DEPLOY_DIR/${dir}', 'zip', '.', '${dir}')"
+    powershell.exe -NoProfile -Command "Compress-Archive -Force -Path '$(cygpath -w "$dir")' -DestinationPath '$(cygpath -w "$DEPLOY_DIR/${dir}.zip")'"
     echo "Packed $DEPLOY_DIR/${dir}.zip"
 done
